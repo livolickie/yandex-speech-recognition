37cd4ccf29962a768f645b45f7c2821f078895f5
livolickie <havingteam@gmail.com>
2019-05-07 21:48:26 +0500

    first

---
+++ Google Speech API/Buffer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+
+namespace Google_Speech_API
+{
+    public class Buffer
+    {
+        MemoryStream memory_data;
+        BinaryWriter write_data;
+        BinaryReader read_data;
+        public Buffer()
+        {
+            memory_data = new MemoryStream();
+            write_data = new BinaryWriter(memory_data);
+            read_data = new BinaryReader(memory_data);
+        }
+        public void Write(int value)
+        {
+            write_data.Write(value);
+            write_data.Flush();
+        }
+        public void Write(float value)
+        {
+            write_data.Write(value);
+            write_data.Flush();
+        }
+        public void Write(double value)
+        {
+            write_data.Write(value);
+            write_data.Flush();
+        }
+        public void Write(long value)
+        {
+            write_data.Write(value);
+            write_data.Flush();
+        }
+        public void Write(string value)
+        {
+            write_data.Write(value);
+            write_data.Flush();
+        }
+        public void Write(bool value)
+        {
+            write_data.Write(value);
+            write_data.Flush();
+        }
+        public void WriteFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                FileStream fl = new FileStream(path, FileMode.Open);
+                write_data.Write((int)fl.Length);
+                write_data.Flush();
+                fl.CopyTo(memory_data);
+                memory_data.Flush();
+                fl.Close();
+            }
+        }
+        public void Seek(int pos)
+        {
+            if (pos > 0 && pos < memory_data.Length)
+            {
+                memory_data.Position = pos;
+            }
+        }
+        public void SeekStart()
+        {
+            memory_data.Position = 0;
+        }
+        public short ReadShort()
+        {
+            return read_data.ReadInt16();
+        }
+        public int ReadInt()
+        {
+            return read_data.ReadInt32();
+        }
+        public double ReadDouble()
+        {
+            return read_data.ReadDouble();
+        }
+        public long ReadLong()
+        {
+            return read_data.ReadInt64();
+        }
+        public string ReadString()
+        {
+            return read_data.ReadString();
+        }
+        public bool ReadBool()
+        {
+            return read_data.ReadBoolean();
+        }
+        public byte[] ReadData(int size)
+        {
+            return read_data.ReadBytes(size);
+        }
+        public FileInfo ReadFile(string path)
+        {
+            FileStream fl = new FileStream(path, FileMode.Create);
+            int size = read_data.ReadInt32();
+            fl.Write(memory_data.ToArray(), (int)memory_data.Position, size);
+            fl.Flush();
+            fl.Close();
+            fl.Dispose();
+            GC.Collect();
+            FileInfo fll = new FileInfo(path);
+            return fll;
+        }
+        public byte[] ReturnData()
+        {
+            return memory_data.ToArray();
+        }
+        public void WriteData(byte[] data)
+        {
+            memory_data.Write(data, 0, data.Length);
+            write_data.Flush();
+            data = null;
+        }
+        public void Clear()
+        {
+            memory_data.Seek(0, SeekOrigin.Begin);
+            memory_data.Flush();
+            write_data.Close();
+            read_data.Close();
+            memory_data.Close();
+            memory_data.Dispose();
+            write_data.Dispose();
+            read_data.Dispose();
+            memory_data = null;
+            write_data = null;
+            read_data = null;
+            GC.Collect();
+        }
+    }
+}
---
+++ Google Speech API/MainWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio.Wave;
+using NAudio.Midi;
+using System.Net;
+using System.IO;
+using System.Xml;
+
+namespace Google_Speech_API
+{
+    public partial class MainWindow : Window
+    {
+        public WaveIn waveIn = null;
+        public WaveStream waveStream = null;
+        public WaveFileWriter waveFileWriter = null;
+        public BufferedWaveProvider bufferedWaveProvider = null;
+        public int tempWav = 0;
+        bool record = false;
+        string key_data = "uuid=&key=&";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!record)
+            {
+                record = true;
+                waveIn = new WaveIn();
+                waveIn.WaveFormat = new WaveFormat();
+             //   waveStream = new WaveFormatConversionStream(waveIn.WaveFormat, new );
+                waveFileWriter = new WaveFileWriter("sound"+tempWav+".wav", waveIn.WaveFormat);
+               // bufferedWaveProvider = new BufferedWaveProvider(waveIn.WaveFormat);
+               //  bufferedWaveProvider.DiscardOnBufferOverflow = true;
+                waveIn.DataAvailable += WaveIn_DataAvailable;
+                waveIn.RecordingStopped += WaveIn_RecordingStopped;
+                waveIn.StartRecording();
+            }
+        }
+
+        public async void SpeachProcess(int fileNum)
+        {
+            string postData = key_data +
+                "topic=queries&lang=ru-RU";
+            string uri = "https://asr.yandex.net/asr_xml?" + postData;
+            HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(uri);
+            httpWebRequest.KeepAlive = false;
+            httpWebRequest.ProtocolVersion = HttpVersion.Version11;
+            httpWebRequest.Method = "POST";
+            httpWebRequest.ContentType = "audio/x-wav";
+            byte[] data = File.ReadAllBytes("sound" + fileNum + ".wav");
+            httpWebRequest.ContentLength = data.Length;
+            Stream requestStream = await httpWebRequest.GetRequestStreamAsync();
+            requestStream.Write(data, 0, data.Length);
+            requestStream.Close();
+            HttpWebResponse httpWebResponse = (HttpWebResponse)await httpWebRequest.GetResponseAsync();
+            XmlDocument xmlDoc = new XmlDocument();
+            string responseText = await new StreamReader(httpWebResponse.GetResponseStream()).ReadToEndAsync();
+            xmlDoc.LoadXml(responseText);
+            foreach (XmlNode node in xmlDoc.SelectNodes("recognitionResults"))
+            {
+                foreach (XmlNode nodeChild in node.ChildNodes)
+                    if (nodeChild.Name == "variant")
+                    {
+                        textBox_1.Text += nodeChild.InnerText + "\n";
+                    }
+            }
+            textBox_1.Text += "\n";
+            File.Delete("sound" + fileNum + ".wav");
+            httpWebResponse.Dispose();
+            GC.Collect();
+        }
+
+        private void WaveIn_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (!record)
+            {
+                waveFileWriter.Close();
+                waveFileWriter.Dispose();
+                waveIn.Dispose();
+                GC.Collect();
+                SpeachProcess(tempWav);
+                tempWav++;
+            }
+            else
+            {
+                SpeachProcess(tempWav);
+                tempWav++;
+                waveIn.Dispose();
+                GC.Collect();
+                waveIn = new WaveIn();
+                waveIn.WaveFormat = new WaveFormat();
+                waveFileWriter = new WaveFileWriter("sound" + tempWav + ".wav", waveIn.WaveFormat);
+                waveIn.DataAvailable += WaveIn_DataAvailable;
+                waveIn.RecordingStopped += WaveIn_RecordingStopped;
+                waveIn.StartRecording();
+
+            }
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            try
+            {
+                if (waveIn != null)
+                {
+                    if (waveFileWriter.Length + e.BytesRecorded < 500000)
+                    {
+                        waveFileWriter.Write(e.Buffer, 0, e.BytesRecorded);
+                        waveFileWriter.Flush();
+                    }
+                    else
+                    {
+                        waveIn.DataAvailable -= WaveIn_DataAvailable;
+                        waveFileWriter.Close();
+                        waveFileWriter.Dispose();
+                        waveIn.StopRecording();
+                        GC.Collect();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                textBox_1.Text += ex;
+            }
+            //bufferedWaveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        private void Button_PreviewMouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            if (record)
+            {
+                record = false;
+                if (waveIn != null)
+                    waveIn.StopRecording();
+            }
+        }
+    }
+}
